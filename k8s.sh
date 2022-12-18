6bd2f27633be4ad7a5c1dcd1a4131ff02a77282d
Ubuntu <ubuntu@ip-172-31-6-154.ec2.internal>
2022-12-18 18:41:16 +0000

    k8s

--- k8s.sh
+++ k8s.sh
@@ -1,1 +1,1 @@
-kubectl create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1
+kubectl create deployment kubernetes-bootcamp --image=https://hub.docker.com/repository/docker/uanozi200/js_server_cw2
